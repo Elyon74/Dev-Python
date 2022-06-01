56b683df3b78020cb90dece93a0fc8a94654d0e8
Elyon74 <antho74140@outlook.fr>
2022-06-01 15:07:24 +0200

    1.01 fix 9
    - Python 3.10.4 update

--- PythonApplication2/src/module1.py
+++ PythonApplication2/src/module1.py
@@ -1,29 +1,15 @@
-#Appel de la bibliothèque
+﻿#Appel de la bibliothèque
 from tkinter import *
 
 #-------------Création de l'interface graphique---------------
 #Création de la fenêtre et de son titre
 
 window = Tk()
-window.title("Python 3.9.10")
-window.geometry("480x360")
-window.minsize(480, 360)
+window.title("Python 3.10.4")
+window.geometry("640x480")
+window.minsize(640, 480)
 window.configure(bg='red')
 window.mainloop()
 user = ""
 
 #Mise en place d'un widget de label (fenetre du programme)
-
-def demander_user():
-    while user == "":
-        user = Label(window, text="Veuillez entrez votre nom d' utilisateur :")
-        user.pack()
-        #Mise en place d'un widget de saisie
-        userenter = StringVar()
-        userenter.set("")
-        userenter2 = Entry(window, textvariable=userenter, width=12)
-        userenter2.pack()
-        #Mise en place d'un widget de bouton
-        boutonuser = Button(window, text="Valider", width=10)
-        boutonuser.pack()
-        demander_user()
